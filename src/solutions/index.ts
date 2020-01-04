bc9a19d29d0fb51147d6ceeb90a65c39e8801b5b
Johny Hoffman <johny.hoffman@gmail.com>
2020-01-03 21:27:07 -0600

    feat: added problem five solution

--- src/solutions/index.ts
+++ src/solutions/index.ts
@@ -2,4 +2,4 @@ export { One } from "./One";
 export { Two } from "./Two";
 export { Three } from "./Three";
 export { Four } from "./Four";
-export { Five } from "./FIve";
+export { Five } from "./Five";
